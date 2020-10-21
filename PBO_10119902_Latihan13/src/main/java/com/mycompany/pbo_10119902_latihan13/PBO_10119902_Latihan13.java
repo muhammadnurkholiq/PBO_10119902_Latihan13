e273fc2fab44c522c8bcee8e3639aa8a203976a4
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-10-21 10:43:29 +0700

    Add files via upload
    First Commit

---
+++ PBO_10119902_Latihan13/src/main/java/com/mycompany/pbo_10119902_latihan13/PBO_10119902_Latihan13.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo_10119902_latihan13;
+
+/**
+ *
+ * @author Muhammad Nurkholiq
+ */
+public class PBO_10119902_Latihan13 {
+    public static void main(String[] args) {
+        int bill = 10;
+        int bill2 = 20;
+        
+        System.out.println("a == b = " + (bill == bill2));
+        System.out.println("a != b = " + (bill != bill2));
+        System.out.println("a > b = " + (bill > bill2));
+        System.out.println("a < b = " + (bill < bill2));
+        System.out.println("a >= b = " + (bill >= bill2));
+        System.out.println("a <= b = " + (bill <= bill2));
+    }
+}
